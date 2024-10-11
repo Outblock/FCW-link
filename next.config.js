3fa6d9cfe87c7471521d680417cade72513752df
lmcmz <lmcmze@gmail.com>
2024-10-11 18:40:37 +1100

    Change header to no cache

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [
+          { key: "Content-Type", value: "application/json" },
+          { key: "Cache-Control", value: "no-cache" },
+        ],
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
